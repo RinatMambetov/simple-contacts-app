b41860cf8d990497aad616088e33dc8a4ff80d95
rinat <mambetovpost@gmail.com>
2024-10-22 17:53:47 +0300

    Commit ot video 2.20

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
+import axios from "axios";
 import "./App.css";
 import FormNewItem from "./layouts/formNewItem/FormNewItem";
 import TableView from "./layouts/tableView/TableView";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState([
-    { id: 1, fullName: "name1", phoneNum: "+7 123 456 78 91", note: "note1" },
-    { id: 2, fullName: "name2", phoneNum: "+7 123 456 78 92", note: "note2" },
-    { id: 3, fullName: "name3", phoneNum: "+7 123 456 78 93", note: "note3" },
-  ]);
+  //   { id: 1, fullName: "name1", phoneNum: "+7 123 456 78 91", note: "note1" },
+  //   { id: 2, fullName: "name2", phoneNum: "+7 123 456 78 92", note: "note2" },
+  //   { id: 3, fullName: "name3", phoneNum: "+7 123 456 78 93", note: "note3" },
+  // ]);
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/contacts").then((res) => {
+      const data = [];
+      res.data._embedded.contacts.forEach((element) => {
+        data.push({
+          fullName: element.fullName,
+          phoneNum: element.phoneNum,
+          note: element.note,
+        });
+      });
+      setData(data);
+    });
+  }, []);
 
   const appendContact = (fullName, phoneNum, note) => {
     const length = data.length;
