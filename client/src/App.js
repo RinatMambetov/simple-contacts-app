0e9eb7b4bd7b686b86c4cfab9aa7871368b5f281
rinat <mambetovpost@gmail.com>
2024-10-22 19:48:24 +0300

    Commit to video 2.23

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,15 @@ import TableView from "./layouts/tableView/TableView";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  //   { id: 1, fullName: "name1", phoneNum: "+7 123 456 78 91", note: "note1" },
-  //   { id: 2, fullName: "name2", phoneNum: "+7 123 456 78 92", note: "note2" },
-  //   { id: 3, fullName: "name3", phoneNum: "+7 123 456 78 93", note: "note3" },
-  // ]);
-
   const [data, setData] = useState([]);
+  const baseUrl = "http://localhost:8080/api/";
 
   useEffect(() => {
-    axios.get("http://localhost:8080/api/contacts").then((res) => {
+    axios.get(baseUrl + "contacts").then((res) => {
       const data = [];
       res.data._embedded.contacts.forEach((element) => {
         data.push({
+          id: element.id,
           fullName: element.fullName,
           phoneNum: element.phoneNum,
           note: element.note,
@@ -27,29 +24,25 @@ function App() {
   }, []);
 
   const appendContact = (fullName, phoneNum, note) => {
-    const length = data.length;
-    let countId = 0;
-    if (length === 0) {
-      countId = 1;
-    } else {
-      countId = data[length - 1].id + 1;
-    }
     const temp = {
-      id: countId,
       fullName: fullName,
       phoneNum: phoneNum,
       note: note,
     };
+    const url = baseUrl + "contacts";
+    axios.post(url, temp);
     setData([...data, temp]); // new array
     // console.log(data.length);
   };
 
   const removeContact = (id) => {
+    const url = baseUrl + `contacts/${id}`;
+    axios.delete(url);
     setData(data.filter((item) => item.id !== id));
   };
 
   return (
-    <div className="container mt-5">
+    <div className="container m-5">
       <div className="card">
         <div className="card-header">
           <h1>List of contacts</h1>
